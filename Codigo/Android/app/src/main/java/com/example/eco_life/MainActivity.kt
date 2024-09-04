5dfbb1cb1071a727b18df520d6a5e670d89c1b57
CharismaticTopHat <a0p5garcia@gmail.com>
2024-09-03 23:37:11 -0600

    "Android - Started main menu"

--- Codigo/Android/app/src/main/java/com/example/eco_life/MainActivity.kt
+++ Codigo/Android/app/src/main/java/com/example/eco_life/MainActivity.kt
@@ -4,44 +4,104 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.eco_life.ui.theme.EcoLifeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             EcoLifeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StartMenu()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StartMenu() {
+    val textSize = 20.sp
+    val textHeight = textSize.value.dp
+    val customColor = Color(30, 132, 73 )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Green)
+            .padding( start = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Inicio",
+                    fontFamily = FontFamily.Serif,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = textSize,
+                    color = Color.White
+                )
+            }
+            Column(
+                modifier = Modifier.weight(1f),
+                horizontalAlignment = Alignment.End
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.eco_espacio),
+                    contentDescription = "Eco Espacio Logo",
+                    modifier = Modifier
+                        .height(textHeight)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 0.dp, top = 8.dp)
+                .background(Color.White),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Recomendaciones",
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = customColor,
+                fontSize = textSize
+            )
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EcoLifeTheme {
-        Greeting("Android")
+        StartMenu()
     }
 }
